223a10eb7488fc2f0072fd2d398076d308effff0
john mwangi <JonesMwas@365@gmail.com>
2019-02-17 02:00:33 +0300

    fonts changes

--- js/triangle.js
+++ js/triangle.js
@@ -1,7 +1,7 @@
 function findout() {
-   var inp1=document.getElementById("sideA").value
-   var inp2=document.getElementById("sideB").value
-   var inp3=document.getElementById("sideC").value
+   var inp1=document.getElementById("data1").value
+   var inp2=document.getElementById("data2").value
+   var inp3=document.getElementById("data3").value
    var sides = ["sideA","sideB","sideC"]
    var sideA=parseInt("sideA");
    var sideB=parseInt("sideB");
@@ -9,19 +9,21 @@ function findout() {
 
     if (sideA === sideB && sideB === sideC); {
       alert("Equilateral Triangle");
-    }else if(sideA !== sideB && sideB === sideC); {
-      alert("Equilateral Triangle");
-    }if (sideA === sideB && != sideC); {
-      alert("Isosceles Triangle");
-    } else if (sideA !== sideB && sideB !== sideC); {
-      alert("Scalene Triangle");
-    } else if (sideA + sideB >= sideC); {
-      alert("Scalene Triangle");
-    } else if (sideA + sideB && >= sideC); {
-      alert("Not a triangle at all");
-    }else if(sideB + sideC && >= sideA); {
-      alert("Not a triangle at all")
-    }else if(sideA + sideC && >= sideB); {
-      alert("Not a triangle at all");
+      else if(sideA !== sideB && sideB === sideC); {
+        alert("Equilateral Triangle");
+      }if (sideA === sideB && != sideC); {
+        alert("Isosceles Triangle");
+      } else if (sideA !== sideB && sideB !== sideC); {
+        alert("Scalene Triangle");
+      } else if (sideA + sideB >= sideC); {
+        alert("Scalene Triangle");
+      } else if (sideA + sideB && >= sideC); {
+        alert("Not a triangle at all");
+      }else if(sideB + sideC && >= sideA); {
+        alert("Not a triangle at all")
+      }else if(sideA + sideC && >= sideB); {
+        alert("Not a triangle at all");
+      }
     }
+
   }
