fc1331500a3dc2aefd05482aee97ae4d93f4d317
john mwangi <JonesMwas@365@gmail.com>
2019-02-18 11:28:08 +0300

    fixing bugs

--- js/triangle.js
+++ js/triangle.js
@@ -13,15 +13,9 @@ function findOut() {
     alert("Isosceles Triangle");
   } else if (sideA !== sideB && sideB !== sideC && sideA !== sideC) {
     alert("Scalene Triangle");
-<<<<<<< HEAD
+
   } else {
     alert("Cannot form a Triangle");
   }
-=======
-  } else if {
-    sideA + sideB <= sideC || sideB + sideC <= sideA || sideA + sideC <= sideB) {
-    alert("Not a triangle");
-  }
 
->>>>>>> gh-pages
 }
