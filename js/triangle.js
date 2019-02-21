1e410d6d41bdb139ddf4b72542f6799b90bfd742
john mwangi <JonesMwas@365@gmail.com>
2019-02-21 11:18:18 +0300

    background view

--- js/triangle.js
+++ js/triangle.js
@@ -1,4 +1,4 @@
--function findOut() {
+function findOut() {
   var inp1 = document.getElementById("data1").value;
   var inp2 = document.getElementById("data2").value;
   var inp3 = document.getElementById("data3").value;
@@ -7,15 +7,19 @@
   var sideB = parseInt(inp2);
   var sideC = parseInt(inp3);
 
-  if (sideA == sideB && sideB == sideC) {
-    alert("Equilateral Triangle");
-  } else if (sideA === sideB || sideB === sideC || sideA === sideC) {
-    alert("Isosceles Triangle");
-  } else if (sideA !== sideB && sideB !== sideC && sideA !== sideC) {
-    alert("Scalene Triangle");
-
+  if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA) {
+    alert("Cannot form a triangle");
   } else {
-    alert("Cannot form a Triangle");
+    if (sideA == sideB && sideB == sideC) {
+      alert("Equilateral Triangle");
+    } else if (sideA === sideB || sideB === sideC || sideA === sideC) {
+      alert("Isosceles Triangle");
+    } else if (sideA !== sideB && sideB !== sideC && sideA !== sideC) {
+      alert("Scalene Triangle");
+
+    } else {
+      alert("Cannot form a Triangle");
+    }
   }
 
 }
